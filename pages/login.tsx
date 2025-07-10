25bbcb4d29f82d4506cbd57cb8a44e16dd7a8a25
CHECKito-Lunch <c.friedrich89@gmail.com>
2025-07-10 11:07:53 +0000

    Fix)

--- pages/login.tsx
+++ pages/login.tsx
@@ -23,7 +23,7 @@ export default function Login() {
       if (role === "admin") {
         router.push("/admin");
       } else {
-        router.push("/dashboard");
+        router.push("/index");
       }
     }
   };
